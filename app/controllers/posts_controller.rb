84efa8858e69bb29cc1aaf084cc89d43045e8f59
B V Satyaram <bvsatyaram@gmail.com>
2015-12-30 13:25:23 +0530

    Add ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @posts = Post.all
+    if params[:mine]
+      @posts = current_user.posts
+    else
+      @posts = Post.all
+    end
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
